6d706a82cb784d6eadf1f47daabc1ba42bde0ddb
capqueen <wzhuangfu@hotmail.com>
2014-09-23 15:59:55 +0800

    建立asp.net mvc 5

---
+++ CapQueen.CMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CapQueen.CMS.Startup))]
+namespace CapQueen.CMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
